c57101fa68a2fe25187224475a5eea46037fdadb
Janpu Hou <JanpuHou@users.noreply.github.com>
2024-11-23 21:00:40 +0800

    Delete load.py

--- load.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-
-loader = PyPDFLoader(
-    file_path = "20220202-alphabet-10k.pdf",
-    extract_images = True,
-    # headers = None
-    # extraction_mode = "plain",
-    # extraction_kwargs = None,
-)
